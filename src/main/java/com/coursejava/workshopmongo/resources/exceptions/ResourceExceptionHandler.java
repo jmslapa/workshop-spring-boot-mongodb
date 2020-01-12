fc2ee263a45e549680a5d3c1886914ee64ca844b
jmslapa <lapeta96@gmail.com>
2020-01-12 17:18:30 -0300

    Obtendo usuario por id

---
+++ src/main/java/com/coursejava/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.coursejava.workshopmongo.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.coursejava.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;		
+		StandardError err = new StandardError(Instant.now(), status.value(), "Not Found", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
