95dca52cc40a651ff21fa1c9c679d2a3c6e10cd2
jmslapa <lapeta96@gmail.com>
2020-01-13 16:00:27 -0300

    Adicionando comentarios aos posts

--- src/main/java/com/coursejava/workshopmongo/domain/Post.java
+++ src/main/java/com/coursejava/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.coursejava.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.coursejava.workshopmongo.dto.AuthorDTO;
+import com.coursejava.workshopmongo.dto.CommentDTO;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Document
@@ -21,6 +24,7 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -72,6 +76,10 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	@Override
